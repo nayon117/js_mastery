0f3cbfc2e5b7f0100bb02a06c4f6a8516db57e21
nayon117 <hasibul.nayon1@gmail.com>
2024-06-30 20:38:53 +0600

    solve map and filter problem

---
+++ 15.js
@@ -0,0 +1,7 @@
+// create a function that takes an array of numbers and return a new array with only the even number;
+
+let arr = [10,15,20,25,30,35,40];
+
+const filterEvenNumber = (arr) =>{
+    return arr.filter(num=>num%2===0)
+}
---
+++ 16.js
@@ -0,0 +1,7 @@
+// use map to double all elements of an array
+
+const doubleArray = (arr) =>{
+    return arr.map(num=> num *2)
+}
+
+let arr = [10,15,20,25,30,35,40];
