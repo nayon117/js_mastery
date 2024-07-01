4bcdd6c9dcc781af665a0cab4036419170a514b1
nayon117 <hasibul.nayon1@gmail.com>
2024-07-01 06:16:06 +0600

    find largest number problem solved

---
+++ 17.js
@@ -0,0 +1,8 @@
+// find the largest element in the array using the reduce method
+
+const findLargestElement = (arr) => {
+  return arr.reduce((max, current) => 
+    current > max ? current : max );
+};
+let arr = [10,15,20,25,30,35,40];
+console.log(findLargestElement(arr));
